99e34a34fdb1ebfe89c2efdbe4654cc21681dd88
Michael Han <han.michael@gmail.com>
2014-12-19 08:28:35 -0800

    Oops, git aliases in the wrong place

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -7,18 +7,13 @@
 # fi
 
 # The rest of my fun git aliases
-# alias gl='git pull --prune'
-# alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-# alias gp='git push origin HEAD'
-# alias gd='git diff'
-# alias gc='git commit'
-# alias gca='git commit -a'
-# alias gco='git checkout'
-# alias gcb='git copy-branch-name'
-# alias gb='git branch'
-# alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
-
-alias lawg="log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
-alias unstage='reset HEAD'
-alias rollback='checkout'
-alias staged-diff='diff --staged'
+alias gl='git pull --prune'
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gp='git push origin HEAD'
+alias gd='git diff'
+alias gc='git commit'
+alias gca='git commit -a'
+alias gco='git checkout'
+alias gcb='git copy-branch-name'
+alias gb='git branch'
+alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
