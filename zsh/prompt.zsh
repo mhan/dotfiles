b712b46124301d308001e40f13dd4dd1a93c9bfa
Michael Han <han.michael@gmail.com>
2014-12-19 09:22:51 -0800

    zsh prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -68,10 +68,10 @@ rb_prompt() {
 }
 
 directory_name() {
-  echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
+  echo "%{$fg_bold[cyan]%}%~%{$reset_color%}"
 }
 
-export PROMPT=$'\n$(rb_prompt)in $(directory_name) $(git_dirty)$(need_push)\n› '
+export PROMPT=$'\n$(rb_prompt)in $(directory_name) $(git_dirty)$(need_push)\n🍖  '
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
 }
